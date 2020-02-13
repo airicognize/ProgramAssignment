63386b0d12c3becfa1e4991934f59c9d5cd910cc
airi <59892547+airicognize@users.noreply.github.com>
2020-02-13 15:45:55 -0500

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+/*We are writing a program to calculate the cost of a roadtrip. We will ask the user how many kilometers will be travelled,
+then ask what is the average distance that the vehicle runs with 1 liter of gas. The program should output
+the estimated cost of the trip, considering the price of gas as $1.16 CAD per liter. 
+*/
+
+public  class Question1{
+	
+	public static void main(String[] args){
+		
+		Scannner sc = Scanner(System.in);
+		double kilometers = Scan.nextDouble();
+		
+		System.out.println("How many kilometers will be travelled?");
+		
+		Scanner sc = Scanner(System.in);
+		double distance = Scan.nextDouble();
+		
+		System.out.println("What is the average distance that your vehicle can do with 1 liter of gas");
+		
+		System.out.println("The estimated cost of the trip will be "(kilmoeters/distance)*1.16)
+		
+	}
+}
