e625ec39918901bcd560c4cdecbecdf7f27b538b
Jeremie <trulyepicman2@gmail.com>
2020-02-13 23:36:30 -0500

    Question 3

---
+++ Question3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.math.*;
+
+public class Question3{
+
+  public static void main(String[] args){
+
+    Scanner in = new Scanner(System.in);
+    int[] numbers = new int[3];
+    int sum = 0;
+
+    System.out.println("\n****** Addition Calculation ******"
+                       + "\nEnter 3 whole digits to calculate an adition");
+    for (int i = 0; i < numbers.length; i++){
+
+
+      System.out.println (/*ask for decreasing*/ "You've entered " + i + " numbers.");
+      numbers[i] = in.nextInt();
+
+
+      try{
+        Thread.sleep(1000);
+      }
+      catch(InterruptedException ex){
+        Thread.currentThread().interrupt();
+      }
+      
+      System.out.println("Calculating");
+
+
+      System.out.println("The total of your numbers are " + numbers);
+
+    }
+
+  }
+
+}
